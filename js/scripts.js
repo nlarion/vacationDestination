a5b51127e539815dc83d5e3e8b490f0f522cc493
Neil Larion <neil.larion@gmail.com>
2016-01-15 12:59:34 -0800

    finish logic for which vacation destination should display based on user input and add content to destination locations.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,44 @@
 $(document).ready(function(){
   $(".carousel").carousel(1);
+  var answersArray = [];
+  hideAll();
+
+  $("#reset").click(function(){
+    $(".carousel").carousel(1);
+  });
+
   $("form").submit(function(event){
-    console.log("awwwww yeah")
+
+    var total = parseInt($("input[name=coffee]:checked").val());
+    total += parseInt($("input[name=water]:checked").val());
+    total += parseInt($("input[name=wakeUp]:checked").val());
+    total += parseInt($("input[name=excitement]:checked").val());
+    total += parseInt($("input[name=sand]:checked").val());
+
+    if(total < 6){
+      hideAll();
+      $(".vla").show();
+    }else if(total > 5 && total < 11){
+      hideAll();
+      $(".deMoines").show();
+    }else {
+      hideAll();
+      $(".risa").show();
+    }
+
+    $(".carousel").carousel(7);
+
+    console.log(total);
     event.preventDefault();
   });
 
 });
+
+
+function hideAll(argument) {
+  $(".vla").hide();
+  $(".deMoines").hide();
+  $(".risa").hide();
+  $(".seattle").hide();
+  $(".moon").hide();
+}
